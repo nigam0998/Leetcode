19af70e23fd032761c55a54228606d7446f8bd4d
Devyansh Nigam <183173932+nigam0998@users.noreply.github.com>
2026-07-05 16:46:08 +0000

    Sync LeetCode submission - Binary Tree Right Side View (python)

---
+++ problems/binary_tree_right_side_view/solution.py
@@ -0,0 +1,18 @@
+from collections import deque
+class Solution:
+    def rightSideView(self, root):
+        if not root:
+            return []
+        ans = []
+        q = deque([root])
+        while q:
+            size = len(q)
+            for i in range(size):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                if i == size - 1:
+                    ans.append(node.val)
+        return ans
