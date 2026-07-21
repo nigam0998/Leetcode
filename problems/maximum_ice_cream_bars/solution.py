dbe83ff8c86fc01813264adf894649b8ab4a573f
Devyansh Nigam <183173932+nigam0998@users.noreply.github.com>
2026-07-21 16:48:07 +0000

    Sync LeetCode submission - Maximum Ice Cream Bars (python)

---
+++ problems/maximum_ice_cream_bars/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxIceCream(self, costs, coins):
+        costs.sort()
+        count = 0
+        for cost in costs:
+            if coins >= cost:
+                coins -= cost
+                count += 1
+            else:
+                break
+        return count
