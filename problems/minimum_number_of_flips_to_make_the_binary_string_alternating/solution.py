65797ee433b3a640d7093c52d3bbc57cdc8391dc
Devyansh Nigam <183173932+nigam0998@users.noreply.github.com>
2026-07-21 17:00:25 +0000

    Sync LeetCode submission - Minimum Number of Flips to Make the Binary String Alternating (python)

---
+++ problems/minimum_number_of_flips_to_make_the_binary_string_alternating/solution.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minFlips(self, s):
+        n = len(s)
+        s = s + s
+        alt1 = ""
+        alt2 = ""
+        for i in range(2 * n):
+            if i % 2 == 0:
+                alt1 += "0"
+                alt2 += "1"
+            else:
+                alt1 += "1"
+                alt2 += "0"
+        diff1 = 0
+        diff2 = 0
+        ans = float("inf")
+        left = 0
+        for right in range(2 * n):
+            if s[right] != alt1[right]:
+                diff1 += 1
+            if s[right] != alt2[right]:
+                diff2 += 1
+            if right - left + 1 > n:
+                if s[left] != alt1[left]:
+                    diff1 -= 1
+                if s[left] != alt2[left]:
+                    diff2 -= 1
+                left += 1
+            if right - left + 1 == n:
+                ans = min(ans, diff1, diff2)
+        return ans
